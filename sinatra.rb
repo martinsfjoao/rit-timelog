5424f792e26d86773c9e833f9cdc5e248f994ef0
João Sampaio Martins <joao.smartins@readinessit.com>
2018-10-25 17:23:55 +0100

    updated login interface

--- sinatra.rb
+++ sinatra.rb
@@ -16,32 +16,33 @@ class App < Sinatra::Base
       puts "Payload received:" 
       puts payload
       
-      # Selenium::WebDriver::PhantomJS.path = 'C:\Users\jmartins\Downloads\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\bin\phantomjs.exe'
-      @driver = Selenium::WebDriver.for :phantomjs
-      @driver.manage.window.set_size 1920, 1080
+      #Selenium::WebDriver::PhantomJS.path = 'C:\Users\jmartins\Downloads\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\bin\phantomjs.exe'
+      @driver = Selenium::WebDriver.for :firefox
+      # @driver.manage.window.resize_to 1300, 500
       puts "Opening TimeLog"
       @driver.navigate.to 'https://app4.timelog.com/readinessit/Registration/TimeTracking'
-      sleep 5 #2
+      sleep 2 #2
       puts "Opened"
-      account_input = @driver.find_element(:id, 'shortname')
-      username_input = @driver.find_element(:id, 'username')
-      password_input = @driver.find_element(:id, 'password')
-      login_button = @driver.find_element(:xpath, '//input[contains(@class,"btn")]')
+	        sleep 10
+      #account_input = @driver.find_element(:id, 'shortname')
+      username_input = @driver.find_element(:id, 'Username')
+      password_input = @driver.find_element(:id, 'Password')
+      login_button = @driver.find_element(:xpath, '//button[contains(@class,"btn")]')
       
       project_inputs = @driver.find_elements(:xpath, '//tr[contains(.,"Panama") and contains(@class,"has-arrow")]//input')
       spanish_ipunts = @driver.find_elements(:xpath, '//tr[contains(.,"Panama") and contains(@class,"has-arrow")]//input')
       
       
-      
-      if account_input.displayed?
+
+      #if username_input.displayed?
           puts "Logging in: #{payload["options"]["username"]}"
-          account_input.send_keys "readinessit"
+          #account_input.send_keys "readinessit"
           username_input.send_keys payload["options"]["username"]
           password_input.send_keys payload["options"]["password"]
           login_button.click
-      end
+      #end
       
-      sleep 5 #2
+      sleep 2 #2
       
       main_search_input = @driver.find_element(:id, 'timeTrackingMainSearch')
       date_input = @driver.find_element(:xpath, './/input[contains(@class,"dateInput")]')
@@ -60,12 +61,12 @@ class App < Sinatra::Base
                   puts "Going to fill the Project: #{project["name"]}"
                   main_search_input.send_keys project["name"]
       
-                  sleep 5 #3
+                  sleep 3 #3
       
                   search_result = @driver.find_element(:class, 'search-result-item')
                   search_result.click
       
-                  sleep 5 #2
+                  sleep 2 #2
       
                   date_to_fill = (datey - datey.wday + 1 + i).strftime('%d/%m/%Y')
                   puts "Going to fill the Day: #{date_to_fill}"
@@ -74,27 +75,27 @@ class App < Sinatra::Base
                   puts "With: #{project["hours"][i]} hours"
                   hours_input.send_keys project["hours"][i]
       
-                  sleep 5 #2
+                  sleep 2 #2
                   puts "Saving hours"
                   save_button.click
               end
           end
       end
       
-      sleep 5 #2
+      sleep 2 #2
       puts "Filled all the hours"
       
       if payload["options"]["close_week"]
           puts "Going to Submit for approval"
           submit_button.click
-          sleep 10 #2
+          sleep 2 #2
           send_button = @driver.find_element(:id, 'btnClosePeriod')
           send_button.click
       else
           puts "Not going to Submit for approval"
       end
       
-      sleep 5 #2
+      sleep 2 #2
 
       puts "Done"
       
